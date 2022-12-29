dbfe68d4c011b40caa58160c72b0ef32fe5c07e0
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2022-12-29 04:14:13 +0400

    Add files via upload

---
+++ project 17 javascript Calculator/main.js
@@ -0,0 +1,11 @@
+function multiply(){
+    let num1 = document.getElementById('num-1').value;
+    let num2 = document.getElementById('num-2').value;
+    document.getElementById('result').innerHTML = num1 * num2;  
+}
+
+function devide(){
+    let num1 = document.getElementById('num-1').value;
+    let num2 = document.getElementById('num-2').value;
+    document.getElementById('result').innerHTML = num1 / num2;  
+}
